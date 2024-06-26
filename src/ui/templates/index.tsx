611f7985f1797ee24cf6ee2a9a3df24076a494fe
Franco Cagliolo <fcagliolo@cocos.capital>
2024-06-26 09:44:37 -0300

    vercel analytics

--- src/ui/templates/index.tsx
+++ src/ui/templates/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { About, Header, Contact, Portfolio, Experience } from "../organisms";
 import { useLocoScroll } from "../../hooks/useLocoScroll";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   useLocoScroll();
 
   return (
     <main className="bg-black text-white" data-scroll-container>
+      <Analytics />
       <Header />
       <About />
       <Experience />
